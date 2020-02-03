a785d78800dfaacae73e08b06c3a48b34622601e
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-02-03 19:30:41 +0300

    #2 License headers!

--- src/main/java/ru/arsysop/liho/check/CommentSearchEngine.java
+++ src/main/java/ru/arsysop/liho/check/CommentSearchEngine.java
@@ -1,3 +1,15 @@
+/********************************************************************************
+ * Copyright (c) 2020 ArSysOp
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   ArSysOp - initial API and implementation
+ ********************************************************************************/
 package ru.arsysop.liho.check;
 
 import java.util.ArrayList;
@@ -21,7 +33,7 @@ public abstract class CommentSearchEngine {
 		if (completed.get()) {
 			throw new IllegalStateException("Cannot update state after it's completed");
 		}
-		updateFound(line); // redesign to avoid temporal coupling here
+		updateFound(line); // TODO: redesign to avoid temporal coupling here. vertical delegation?
 		updateContent(line);
 		updateCompleted(line);
 	}
