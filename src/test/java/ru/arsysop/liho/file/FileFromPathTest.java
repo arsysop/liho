3339b2411e0fe12c432a2dba79dc4a706cfa5e82
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-02-04 12:27:14 +0300

    Fully test FileFromPath contract

--- src/test/java/ru/arsysop/liho/file/FileFromPathTest.java
+++ src/test/java/ru/arsysop/liho/file/FileFromPathTest.java
@@ -28,44 +28,50 @@ class FileFromPathTest {
 
 	@Test
 	@DisplayName("file extension is parsed correctly")
-	void parseExtension(){
+	void parseExtension() {
 		assertEquals("java", complete.file().extension().get());
 	}
 
 	@Test
 	@DisplayName("file name is parsed correctly")
-	void parseName(){
+	void parseName() {
 		assertEquals("B", complete.file().name());
 	}
 
 	@Test
 	@DisplayName("file parent folder is parsed correctly")
-	void parseFolder(){
+	void parseFolder() {
 		assertEquals(complete.path().getParent(), complete.file().folder());
 	}
 
 	@Test
 	@DisplayName("name is parsed correctly for no-name file")
-	void parseNoNameFileForName(){
+	void parseNoNameFileForName() {
 		assertTrue(noName.file().name().isEmpty());
 	}
 
 	@Test
 	@DisplayName("extension is parsed correctly for no-name file")
-	void parseNoNameFileForExtension(){
+	void parseNoNameFileForExtension() {
 		assertEquals("rc", noName.file().extension().get());
 	}
 
 	@Test
 	@DisplayName("name is parsed correctly for no-extension file")
-	void parseNoExtensionFileForName(){
+	void parseNoExtensionFileForName() {
 		assertEquals("gradlew", noExtension.file().name());
 	}
 
 	@Test
 	@DisplayName("extension is parsed correctly for no-extension file")
-	void parseNoExtensionFileForExtension(){
+	void parseNoExtensionFileForExtension() {
 		assertTrue(noExtension.file().extension().get().isEmpty());
+	}
+
+	@Test
+	@DisplayName("origin is practically equal to the source")
+	void origin() {
+		assertEquals(noExtension.path(), noExtension.file().origin());
 	}
 
 }
